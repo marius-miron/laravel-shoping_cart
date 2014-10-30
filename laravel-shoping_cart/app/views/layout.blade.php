389f6cedda9a7d3ba9cafb1761658d39cac0b31a
Marius Miron <marius.costel.miron@gmail.com>
2014-10-30 17:30:25 +0200

    creating the layout

---
+++ laravel-shoping_cart/app/views/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://code.jquery.com/jquery.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        
+        {{ HTML::script('js/shoping_cart.js') }}
+        {{ HTML::style('css/stylesheet.css') }}
+
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
